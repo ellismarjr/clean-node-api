7f71d43261d81affe235733d6ec24fac126c62eb
Junior de Oliveira <ellismarjr@hotmail.com>
2021-06-14 11:43:09 -0300

    ensure LoginRouter call EmailValidator with correct email

--- src/presentation/routes/loginRouter.spec.js
+++ src/presentation/routes/loginRouter.spec.js
@@ -19,6 +19,7 @@ const makeSut = () => {
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -235,5 +236,17 @@ describe('Login Router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
